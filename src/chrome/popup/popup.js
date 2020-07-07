873762f0cd128d49e7156f3c211a4d38e3d54c54
Pedro Vitor Piassa <33137099+pvpscript@users.noreply.github.com>
2020-07-07 00:05:33 -0300

    Added popup script communication skeleton

--- src/chrome/popup/popup.js
+++ src/chrome/popup/popup.js
@@ -1,18 +1,19 @@
-const submit = document.getElementById("submit");
 const form = document.getElementById("opt-form");
-
 const fgroup = document.getElementById("field-group");
 const ptext = document.getElementById("plaintext");
 const ewin = document.getElementById("ewin");
 const raw = document.getElementById("raw");
 
-submit.addEventListener("click", (e) => {
-	alert("Doing stuff");
-});
-
 form.addEventListener("submit", (e) => {
 	e.preventDefault();
-	console.log(e.target);
+
+	const elements = e.target.elements;
+	console.log(elements);
+
+	chrome.runtime.sendMessage({
+		wOpt: elements['cwin'].checked,
+		type: null
+	});
 });
 
 fgroup.addEventListener("change", (e) => {
